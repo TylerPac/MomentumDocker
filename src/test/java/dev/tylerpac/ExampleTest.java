b13a293e3bb2f636bfb9c4fc58fb81df00023c66
TylerPac <93409601+TylerPac@users.noreply.github.com>
2025-04-18 13:37:16 -0400

    JunitTesting working

--- src/test/java/dev/tylerpac/ExampleTest.java
+++ src/test/java/dev/tylerpac/ExampleTest.java
@@ -5,11 +5,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExampleTest {
-/*
     @Test
     public void testFailingCase() {
-        assertEquals(1, 2, "This test is meant to fail");
+        assertEquals(1, 2-1, "Example test");
     }
-
- */
 }
