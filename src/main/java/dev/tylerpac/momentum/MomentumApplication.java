3c44ccb24bcbb96af5d25e5eb94b1f54dce068a0
TylerPac <93409601+TylerPac@users.noreply.github.com>
2025-06-11 00:54:42 -0400

    Initial push for Springboard testing jenkins and docker push

--- src/main/java/dev/tylerpac/momentum/MomentumApplication.java
+++ src/main/java/dev/tylerpac/momentum/MomentumApplication.java
@@ -2,17 +2,11 @@ package dev.tylerpac.momentum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class MomentumApplication extends SpringBootServletInitializer {
+public class MomentumApplication{
     public static void main(String[] args) {
         SpringApplication.run(MomentumApplication.class, args);
-    }
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(MomentumApplication.class);
     }
 
 }
