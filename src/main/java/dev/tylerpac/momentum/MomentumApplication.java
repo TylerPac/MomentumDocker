bda85f08bbcf8ca74ca4b69fb8ae9352fdc0835d
TylerPac <93409601+TylerPac@users.noreply.github.com>
2025-06-15 19:40:00 -0400

    jar attempt

--- src/main/java/dev/tylerpac/momentum/MomentumApplication.java
+++ src/main/java/dev/tylerpac/momentum/MomentumApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "dev.TylerPac")
 public class MomentumApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
