08c06f5a98a1d991a93dd7540a4437e64dbc0102
TylerPac <93409601+TylerPac@users.noreply.github.com>
2026-01-01 21:38:19 -0500

    Update SecurityConfig.java

--- backend/src/main/java/dev/tylerpac/momentum/security/SecurityConfig.java
+++ backend/src/main/java/dev/tylerpac/momentum/security/SecurityConfig.java
@@ -72,6 +72,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cors = new CorsConfiguration();
         cors.setAllowedOrigins(List.of(
+                "https://momentum.tylerpac.dev",
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
                 "http://localhost:3000",
